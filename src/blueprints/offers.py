f8c0da443a0dc00e5bf497996dc7cd4b9e02cabe
tuch-ka <tuch_ka@mail.ru>
2020-09-02 10:08:07 +0500

    add: offer create

--- src/blueprints/offers.py
+++ src/blueprints/offers.py
@@ -1,20 +1,27 @@
 from sanic import Blueprint
-from sanic.response import json
+from sanic.response import json, empty
 
 from schemas.offer import OfferCreateSchema
 from marshmallow import ValidationError
+
+from models.offer import Offer
 
 bp = Blueprint('offers')
 
 
 @bp.post('/create')
 async def create_offer(request):
+    """Создание объявления. TODO: добавить проверку авторизации"""
     try:
         offer_data = OfferCreateSchema().load(request.json)
     except ValidationError as error:
         return json(error.messages, status=400)
 
-    return json(offer_data, status=200)
+    offer = await Offer.create(data=offer_data)
+    if offer is None:
+        return empty(400)
+
+    return json(offer.to_dict(), status=200)
 
 
 @bp.post('/')
--- src/models/offer.py
+++ src/models/offer.py
@@ -1,6 +1,10 @@
+from typing import Optional
+
 import peewee
+from peewee import IntegrityError
 
 from . import BasicModel
+from database import manager
 
 
 class User(BasicModel):
@@ -14,9 +18,24 @@ class User(BasicModel):
 class Offer(BasicModel):
     """Модель объявления"""
     offer_id = peewee.AutoField()
-    user_id = peewee.ForeignKeyField(User, backref='offers')
+    user_id = peewee.ForeignKeyField(User, backref='offers', field='user_id')
     title = peewee.CharField()
     text = peewee.CharField()
 
     def __repr__(self):
         return f'Offer: {self.title} ({self.user_id})'
+
+    @classmethod
+    async def create(cls, data: dict) -> Optional['Offer']:
+        try:
+            return await manager.create(cls, **data)
+        except IntegrityError:
+            return None
+
+    def to_dict(self):
+        return {
+            'offer_id': self.offer_id,
+            'user_id': self.user_id.user_id,
+            'title': self.title,
+            'text': self.text,
+        }
