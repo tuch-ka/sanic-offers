d97657c8a757d97e163a059ccf7d6652729d4c0a
tuch-ka <tuch_ka@mail.ru>
2020-09-02 11:15:56 +0500

    add: authorization

---
+++ src/auth.py
@@ -0,0 +1,30 @@
+from functools import wraps
+from typing import Optional
+
+import jwt
+from sanic.response import empty
+
+from config import TOKEN_KEY
+
+
+def auth_required(func):
+    @wraps(func)
+    async def wrapper(request, *args, **kwargs):
+
+        token = request.cookies.get('token', None)
+        token_payload = read_token(token)
+
+        if token is None or token_payload is None:
+            response = empty(401)
+        else:
+            response = await func(request, token_payload, *args, **kwargs)
+
+        return response
+    return wrapper
+
+
+def read_token(token) -> Optional[dict]:
+    try:
+        return jwt.decode(token, TOKEN_KEY)
+    except jwt.exceptions.DecodeError:
+        return None
--- src/blueprints/offers.py
+++ src/blueprints/offers.py
@@ -6,16 +6,22 @@ from marshmallow import ValidationError
 
 from models.offer import Offer
 
+from auth import auth_required
+
 bp = Blueprint('offers')
 
 
 @bp.post('/create')
-async def create_offer(request):
-    """Создание объявления. TODO: добавить проверку авторизации"""
+@auth_required
+async def create_offer(request, token_payload):
+    """Создание объявления."""
     try:
         offer_data = OfferCreateSchema().load(request.json)
     except ValidationError as error:
         return json(error.messages, status=400)
+
+    if offer_data['user_id'] != token_payload['user_id']:
+        return empty(403)
 
     offer = await Offer.create(data=offer_data)
     if offer is None:
